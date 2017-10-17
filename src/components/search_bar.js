aeb42d36973fd238b86ce7bd45372cd2f9d8298d
Kurt Schneider <k.w.schneid@gmail.com>
2017-10-17 09:36:15 -0400

    video 17

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 	render() {
-		return <input />;
+		return <input onChange={ e => console.log(e.target.value) } />;
 	}
 }
 
