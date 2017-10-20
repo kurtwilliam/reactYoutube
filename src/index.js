09540d7e22ab4253ddffbb3a2d46ee9d132557c9
Kurt Schneider <k.w.schneid@gmail.com>
2017-10-19 22:51:20 -0400

    Added youtube video list

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
-//Create a newcomponent, whichwillproduce somehtml
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDV1aaSFonkv8aL4ak-OxM5GM2B_qMF9B0';
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
+			this.setState({ videos })
+		})
+	}
+	render() {		
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
 // take this components generated html and put on page (in dom)
