fa41c12d04da6cce7ad99d24c16391ee068d7786
Kurt Schneider <k.w.schneid@gmail.com>
2017-10-22 11:15:51 -0400

    Wrapped up project

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,10 +8,18 @@ class SearchBar extends Component {
 	}
 	render() {
 		return (
-			<div>
-				<input onChange={ e => this.setState({term: e.target.value}) } />
+			<div className="search-bar">
+				<input 
+					value={this.state.term}
+					onChange={ e => this.onInputChange(e.target.value) } 
+				/>
 			</div>
 		);
+	}
+
+	onInputChange(term) {
+		this.setState({term});
+		this.props.onSearchTermChange(term);
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -1,29 +1,48 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 const API_KEY = 'AIzaSyDV1aaSFonkv8aL4ak-OxM5GM2B_qMF9B0';
 
 class App extends Component {
 	constructor(props){
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [], 
+			selectedVideo: null,
+		};
 
-		YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
-			this.setState({ videos })
+		this.videoSearch('surfboards')
+	}
+
+	videoSearch(term){
+		YTSearch({key:API_KEY, term:term}, (videos) => {
+			this.setState({ 
+				videos:videos,
+				selectedVideo:videos[0],
+			})
 		})
 	}
+
 	render() {		
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
 		return (
 			<div>
-				<SearchBar />
-				<VideoList videos={this.state.videos} />
+				<SearchBar onSearchTermChange={ videoSearch } />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} 
+				/>
 			</div>
 		);
 	}
 }
 
-// take this components generated html and put on page (in dom)
 ReactDOM.render(<App />, document.querySelector('.container'));
